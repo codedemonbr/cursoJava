479498b0be6bc1bb30148e354780cc5f9f9361df
Thiago Henrique dos Santos <henrique.thsantos.ths@gmail.com>
2023-11-01 18:06:23 -0300

    Resolvendo desafio map

--- src/streams/DesafioMap.java
+++ src/streams/DesafioMap.java
@@ -2,16 +2,53 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class DesafioMap {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
+        Consumer<Object> println = System.out::println;
 
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-        Function<Integer, String> resto = (n) -> (n % 2 == 1) ? "1" : "0";
+        /*
+         * 1. Numero para string binaria... 6=> "110"
+         * 2. Inverter a string... "110"=> "011"
+         * 3. Converter de volta para inteiro => "011" => 3
+         * 
+         * 
+         */
+        UnaryOperator<String> zeroEsquerda = (str) -> {
+            switch (str.length()) {
+                case 1:
+                    return "000" + str;
+                case 2:
+                    return "00" + str;
+                case 3:
+                    return "0" + str;
+                default:
+                    return str;
+            }
+        };
 
-        System.out.println(resto.apply(6));
+        Function<Integer, String> convertStrBin = (n) -> Integer.toBinaryString(n);
+        UnaryOperator<String> reverseStr = (str) -> new StringBuilder().append(str).reverse().toString();
+        Function<String, Integer> convertBinStr = (str) -> Integer.parseInt(str, 2);
+
+        // System.out.println(convertStrBin.apply(7));
+        // System.out.println(reverseStr.apply("meu teste doido"));
+        // System.out.println(convertBinStr.apply("111"));
+
+        // System.out.println(con);
+
+        nums.stream().map(convertStrBin).map(zeroEsquerda).map(reverseStr).map(convertBinStr).forEach(println);
+        nums.stream().map(Integer::toBinaryString).map(zeroEsquerda).map(reverseStr).map(convertBinStr)
+                .forEach(System.out::println);
+        // nums.stream().map(convertStrBin).map(zeroEsquerda).forEach(println);
     }
 
 }
